3fc52214e902748c7debace0d8540975ba15764d
Keir Smith <keirl.smith@gmail.com>
2022-05-19 19:32:09 -0400

    ContactForm Parts 1 and 2

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,5 +1,6 @@
 import { Container, Col, Row } from 'reactstrap';
 import SubHeader from '../components/SubHeader';
+import ContactForm from '../components/ContactForm';
 
 
 
@@ -34,7 +35,7 @@ const ContactPage = () => {
                     <hr />
                 </Col>
                 <Col md='10'>
-                    TBD: ContactForm
+                    <ContactForm />
                 </Col>
             </Row>
         </Container>
